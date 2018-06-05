68b4ee3aa25ccc2dc340f237d2816f393d41caaf
yasar kumru <yasar.kumru@mebitech.com>
2018-06-05 10:22:20 +0300

    .

--- ramazanTez/src/main/java/io/yasar/utils/DemandService.java
+++ ramazanTez/src/main/java/io/yasar/utils/DemandService.java
@@ -2,25 +2,20 @@ package io.yasar.utils;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.springframework.stereotype.Service;
 
 import io.yasar.model.Demand;
 
-
 @Service
 public final class DemandService {
 
-	public static List<Demand> mergeAll(List<Demand> demands) {
-		for (int i = 0; i < demands.size(); i++) {
-			Demand demand1 = demands.get(i);
-			for (int j = i + 1; j < demands.size(); j++) {
-				Demand demand2 = demands.get(j);
-				demand1.merge(demand2);
-			}
-
-		}
-		return demands.stream().filter(demand -> demand.getValue() > 0)
-				.collect(Collectors.toList());
-	}
+    public static List<Demand> mergeAll(List<Demand> demands) {
+        IntStream.range(0, demands.size())
+                .forEach(i -> IntStream.range(i + 1, demands.size())
+                        .forEach(j -> demands.get(i).merge(demands.get(j))));
+        return demands.stream().filter(demand -> demand.getValue() > 0)
+                .collect(Collectors.toList());
+    }
 }
