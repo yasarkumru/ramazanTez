c99896488b031d6f9e496da52594e87a4932d547
yasarkumru <yasarkumru@gmail.com>
2018-06-06 11:21:59 +0300

    alterations

--- ramazanTez/src/main/java/io/yasar/utils/CalculationService.java
+++ ramazanTez/src/main/java/io/yasar/utils/CalculationService.java
@@ -13,69 +13,65 @@ import io.yasar.model.Tour;
 @Service
 public class CalculationService {
 
-    public List<Solution> run(Solution solution) {
-        List<Solution> solutions = new ArrayList<>();
-        List<Solution> allCurrents = new ArrayList<>();
-        Solution current = new Solution(solution);
+	public List<Solution> run(Solution solution) {
+		List<Solution> solutions = new ArrayList<>();
+		List<Solution> allCurrents = new ArrayList<>();
+		Solution current = new Solution(solution);
 
-        while (true) {
-            allCurrents.add(current);
-            if (current.isFeasible()) {
-                solutions.add(current);
-                Optional<Solution> nextSolution = trySlidingFractionalTours(current);
-                if (!nextSolution.isPresent())
-                    break;
-                Solution nextSolutionObj = nextSolution.get();
-                if(current.isSamePerf(nextSolutionObj))
-                	break;
-                current = nextSolutionObj;
-            } else {// if not feasible
-                Optional<Solution> nextSolution = trySlidingForTimeSlot(current, current.getTheWorstTimeSlot());
-                if(!nextSolution.isPresent())
-                	break;
-                
-                Solution nextSolutionObj = nextSolution.get();
-                if(current.isSamePerf(nextSolutionObj))
-                	break;
-				current = nextSolutionObj;
-            }
-        }
-        return solutions;
-    }
+		while (true) {
+			allCurrents.add(current);
+			Optional<Solution> nextSolution = null;
+			if (current.isFeasible()) {
+				solutions.add(current);
+				nextSolution = trySlidingFractionalTours(current);
+			} else {// if not feasible
+				final TimeSlot theWorstTimeSlot = current.getTheWorstTimeSlot();
+				nextSolution = trySlidingForTimeSlot(current,
+						theWorstTimeSlot);
+			}
+			if (!nextSolution.isPresent())
+				break;
+			Solution nextSolutionObj = nextSolution.get();
+			if (current.isSamePerf(nextSolutionObj))
+				break;
+			current = nextSolutionObj;
+		}
+		return solutions;
+	}
 
-    /**
-     * tries sliding every fractional tour in the solution. returns the one that adds minimum
-     * "tour" Runs for feasible solutions
-     * 
-     * @param current
-     * @return
-     */
-    private static Optional<Solution> trySlidingFractionalTours(Solution current) {
-        return current.getTimeSlots()
-                .stream()
-                .skip(2)
-                .flatMap(ts -> ts.getTours().stream())
-                .filter(Tour::isFractional)
-                .map(current::slideTour)
-                .sorted((s1, s2) -> s1.getPriorityHeuristic() - s2.getPriorityHeuristic())
-                .findFirst();
-    }
+	/**
+	 * tries sliding every fractional tour in the solution. returns the one that adds
+	 * minimum "tour" Runs for feasible solutions
+	 * 
+	 * @param current
+	 * @return
+	 */
+	private static Optional<Solution> trySlidingFractionalTours(Solution current) {
+		return current.getTimeSlots()
+				.stream()
+				.skip(2)
+				.flatMap(ts -> ts.getTours().stream())
+				.filter(Tour::isFractional)
+				.map(current::slideTour)
+				.sorted((s1, s2) -> s1.getPriorityHeuristic() - s2.getPriorityHeuristic())
+				.findFirst();
+	}
 
-    /**
-     * tries sliding every tour in that time slot. returns the one that adds minimum
-     * "line side". Runs for infeasible solutions
-     * 
-     * @param solution
-     * @param timeSlot
-     * @return
-     */
-    private static Optional<Solution> trySlidingForTimeSlot(Solution solution, TimeSlot timeSlot) {
-        return timeSlot.getTours().stream()
-                .map(solution::slideTour)
-                .sorted((s1, s2) -> s1.getTotalLineSideDif() == s2.getTotalLineSideDif()
-                        ? s1.getTotalTourCount() - s2.getTotalTourCount()
-                        : s1.getTotalLineSideDif() - s2.getTotalLineSideDif())
-                .findFirst();
-    }
+	/**
+	 * tries sliding every tour in that time slot. returns the one that adds minimum
+	 * "line side". Runs for infeasible solutions
+	 * 
+	 * @param solution
+	 * @param timeSlot
+	 * @return
+	 */
+	private static Optional<Solution> trySlidingForTimeSlot(Solution solution, TimeSlot timeSlot) {
+		return timeSlot.getTours().stream()
+				.map(solution::slideTour)
+				.sorted((s1, s2) -> s1.getTotalLineSideDif() == s2.getTotalLineSideDif()
+						? s1.getTotalTourCount() - s2.getTotalTourCount()
+						: s1.getTotalLineSideDif() - s2.getTotalLineSideDif())
+				.findFirst();
+	}
 
 }
