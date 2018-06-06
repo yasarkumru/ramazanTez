7d59566e1f6a3545ff639956aaa2ee4fb26a9db3
yasar kumru <yasar.kumru@mebitech.com>
2018-06-06 15:58:30 +0300

    disable command logging

---
+++ ramazanTez/src/main/java/io/yasar/commands/NoSaveHistory.java
@@ -0,0 +1,19 @@
+package io.yasar.commands;
+
+import java.io.IOException;
+
+import org.jline.reader.impl.history.DefaultHistory;
+import org.springframework.stereotype.Component;
+
+/**
+ * This class is for disabling command log
+ *
+ * @author yasar
+ */
+@Component
+public class NoSaveHistory extends DefaultHistory {
+    @Override
+    public void save() throws IOException {
+
+    }
+}
