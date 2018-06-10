5e4844e6b06fce482e82bcae485cc7cd2cea47da
kzl <kzl@DESKTOP-K8AET9R>
2018-06-10 18:49:09 +0300

    .

--- ramazanTez/src/main/java/tr/com/metu/ramazan/excel/Constants.java
+++ ramazanTez/src/main/java/tr/com/metu/ramazan/excel/Constants.java
@@ -3,10 +3,10 @@ package tr.com.metu.ramazan.excel;
 public final class Constants {
 
 
-	public static final int STATION_COUNT = 10;
-	public static final int TIME_SLOT_COUNT = 4;
-	public static final int MAX_TOUR_PER_TIME_SLOT = 4;
-	public static final String FILE_PATH = "ProductionData0.xlsx";
+	public static final int STATION_COUNT = 15;
+	public static final int TIME_SLOT_COUNT = 7;
+	public static final int MAX_TOUR_PER_TIME_SLOT = 6;
+	public static final String FILE_PATH = "Set8.xlsx";
 
 	
 //	public static final int STATION_COUNT = 52;
